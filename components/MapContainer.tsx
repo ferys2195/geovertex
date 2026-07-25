fdb41c8206a7379c828918125bae4b23ccf24b2b
ferys2195 <fery21irawan.me@gmail.com>
2026-07-25 17:33:15 +0700

    feat: configure maxZoom 24 and maxNativeZoom scaling for all basemap tile layers

--- components/MapContainer.tsx
+++ components/MapContainer.tsx
@@ -36,30 +36,40 @@ const BASE_LAYERS = [
     name: 'Standard OpenStreetMap',
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: '&copy; OpenStreetMap contributors',
+    maxNativeZoom: 19,
+    maxZoom: 24,
   },
   {
     id: 'satellite',
     name: 'Esri Satellite Imagery',
     url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    maxNativeZoom: 19,
+    maxZoom: 24,
   },
   {
     id: 'carto-dark',
     name: 'CartoDB Dark Matter',
     url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxNativeZoom: 20,
+    maxZoom: 24,
   },
   {
     id: 'carto-light',
     name: 'CartoDB Positron (Minimal)',
     url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    maxNativeZoom: 20,
+    maxZoom: 24,
   },
   {
     id: 'topo',
     name: 'OpenTopoMap (Kontur)',
     url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
     attribution: 'Kartendaten: &copy; OpenStreetMap-Mitwirkende, SRTM | Kartendarstellung: &copy; OpenTopoMap (CC-BY-SA)',
+    maxNativeZoom: 17,
+    maxZoom: 24,
   }
 ];
 
@@ -409,8 +419,8 @@ export default function MapContainer({
     const defaultLayer = BASE_LAYERS.find(l => l.id === activeLayerId) || BASE_LAYERS[0];
     const tileLayer = L.tileLayer(defaultLayer.url, {
       attribution: defaultLayer.attribution,
-      maxZoom: 24,
-      maxNativeZoom: 19
+      maxZoom: defaultLayer.maxZoom || 24,
+      maxNativeZoom: defaultLayer.maxNativeZoom || 19,
     }).addTo(map);
     activeTileLayerRef.current = tileLayer;
 
@@ -907,8 +917,8 @@ export default function MapContainer({
 
     const nextTileLayer = L.tileLayer(selectedLayer.url, {
       attribution: selectedLayer.attribution,
-      maxZoom: 24,
-      maxNativeZoom: 19
+      maxZoom: selectedLayer.maxZoom || 24,
+      maxNativeZoom: selectedLayer.maxNativeZoom || 19,
     }).addTo(map);
 
     activeTileLayerRef.current = nextTileLayer;
